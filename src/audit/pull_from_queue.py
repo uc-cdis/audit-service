de217b8f8e3646965921d810c6726a5cb042832c
Sai Shanmukha <nss10@outlook.com>
2025-04-04 16:28:16 -0500

    Update pull from queue to have DataAccessLayer

--- src/audit/pull_from_queue.py
+++ src/audit/pull_from_queue.py
@@ -7,9 +7,14 @@ from . import logger
 from .config import config
 from .models import CATEGORY_TO_MODEL_CLASS
 from .routes.maintain import insert_row, validate_presigned_url_log, validate_login_log
+from .routes.maintain import get_data_access_layer
+from .routes.maintain import DataAccessLayer
+from fastapi import Depends
 
 
-async def process_log(data, timestamp):
+async def process_log(
+    data, timestamp, dal: DataAccessLayer = Depends(get_data_access_layer)
+):
     # check log category
     category = data.pop("category")
     assert (
@@ -23,11 +28,13 @@ async def process_log(data, timestamp):
     # validate log
     if category == "presigned_url":
         validate_presigned_url_log(data)
+        dal.create_presigned_url_log(data)
     elif category == "login":
         validate_login_log(data)
+        dal.create_login_log(data)
 
     # insert log in DB
-    await insert_row(category, data)
+    # await insert_row(category, data)
 
 
 async def pull_from_queue(sqs):
